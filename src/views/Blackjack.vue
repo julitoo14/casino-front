a101ea1632ef3e05affb7b57cdc026cd9c3d6fe2
julitoo14 <juuligarcia2208@gmail.com>
2024-05-13 09:45:55 -0300

    added hit and stand buttons

--- src/views/Blackjack.vue
+++ src/views/Blackjack.vue
@@ -5,6 +5,10 @@ import axios from 'axios';
 
 const croupierHand = ref([]);
 const playerHand = ref([]);
+const playing = ref(false);
+const resultado = ref('');
+const puntajeCroupier = ref(0);
+const puntajeJugador = ref(0);
 const config = {headers: {
     Authorization: `${localStorage.getItem('token')}`,
   }
@@ -19,6 +23,35 @@ const play = async () => {
   localStorage.setItem('currentGame', res.data._id);
   croupierHand.value = res.data.manoCroupier;
   playerHand.value = res.data.manoJugador;
+  playing.value = true;
+  resultado.value = '';
+  puntajeJugador.value = res.data.puntajeJugador;
+  puntajeCroupier.value = res.data.puntajeCroupier;
+}
+
+const hit = async () => {
+  const currentGame = localStorage.getItem('currentGame');
+  const res = await axios.put(`http://localhost:3000/api/hit/${currentGame}`,{}, config);
+  croupierHand.value = res.data.manoCroupier;
+  playerHand.value = res.data.manoJugador;
+  puntajeJugador.value = res.data.puntajeJugador;
+  puntajeCroupier.value = res.data.puntajeCroupier;
+  if (res.data.resultado === 'PERDIDA'){
+    playing.value = false;
+    resultado.value = res.data.resultado;
+  }
+}
+
+const stand = async () => {
+  const currentGame = localStorage.getItem('currentGame');
+  const res = await axios.put(`http://localhost:3000/api/stand/${currentGame}`,{}, config);
+  croupierHand.value = res.data.manoCroupier;
+  playerHand.value = res.data.manoJugador;
+  console.log(res);
+  playing.value = false;
+  resultado.value = res.data.resultado;
+  puntajeJugador.value = res.data.puntajeJugador;
+  puntajeCroupier.value = res.data.puntajeCroupier;
 }
 
 </script>
@@ -26,20 +59,29 @@ const play = async () => {
 <template>
     <div class="game-screen">
 
-      <button class="play-button" @click="play">Play</button>
-
       <div class="croupier-hand">
-        <div v-for="card in croupierHand" :key="card._id">
-          <Card :suit="card.palo" :value="card.figura"></Card>
+        <div class="cards">
+          <div v-for="card in croupierHand" :key="card._id">
+            <Card :suit="card.palo" :value="card.figura"></Card>
+          </div>
         </div>
+        <h2>{{puntajeCroupier}}</h2>
       </div>
 
       <div class="player-hand">
-        <div v-for="card in playerHand" :key="card._id" >
-          <Card :suit="card.palo" :value="card.figura"></Card>
+        <div class="cards">
+          <div v-for="card in playerHand" :key="card._id" >
+            <Card :suit="card.palo" :value="card.figura"></Card>
+          </div>
         </div>
+        <h2>{{puntajeJugador}}</h2>
+
       </div>
 
+      <h2 v-if="resultado">{{resultado}}</h2>
+      <button v-if="playing" class="play-button" @click="hit">Hit</button>
+      <button v-if="playing" class="play-button" @click="stand">Stand</button>
+      <button v-else class="play-button" @click="play">Play</button>
 
     </div>
 </template>
@@ -68,8 +110,13 @@ const play = async () => {
   margin: 10px;
   display: flex;
   gap: 10px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 
-
-
+.cards{
+  display: flex;
+  gap: 10px;
+}
 </style>
